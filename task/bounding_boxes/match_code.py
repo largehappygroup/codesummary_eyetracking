f374e8028a484a8dea0b8ad142f417b3b68b4609
birkenstonks <z.karas@vanderbilt.edu>
2023-03-16 19:13:33 -0700

    close matches for OCR words

--- task/bounding_boxes/match_code.py
+++ task/bounding_boxes/match_code.py
@@ -1,13 +1,34 @@
 import difflib
+import re
+import os
+import pandas as pd
 
-large_string = 'the quick brown fox jumps over the lazy dog'
+oracle = pd.read_csv('../stimuli/pruned_seeds2.csv')
+strings = pd.read_csv('./setSecurityMode_boxes.csv')
 
+
+curr = oracle['function'][0]
+split_string = re.split(string=curr, pattern=" |\n")
+split_string = [i for i in split_string if i]
+print(split_string)
+
+#large_string = 'the quick brown fox jumps over the lazy dog'
+#split_string = re.split(pattern=" ", string=large_string)
 # A string to compare to the large string
-string_to_compare = 'thee quick brown fax jump'
+#string_to_compare = 'thee'
 
-# Get the closest matches
-closest_matches = difflib.get_close_matches(
-    string_to_compare, [large_string], n=1)
+for word in strings['word']:
+    #print(word)
+    closest_matches = difflib.get_close_matches(word, split_string, n=3)
+    try:
+        print(word, closest_matches[0])
+    except:
+        print('-------------------')
+        
 
 # Print the closest match
-print(closest_matches[0])
+#print(closest_matches[0])
+
+# Use regex to split string based on splits done by OCR for bounding boxes. 
+# by spaces, not by periods. 
+
