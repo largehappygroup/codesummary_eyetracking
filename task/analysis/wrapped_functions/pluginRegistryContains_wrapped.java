5d073db358e67f78990f08c81665db11155ca89d
birkenstonks <z.karas@vanderbilt.edu>
2023-06-04 19:20:59 -0700

    finished aligning files between parsing and bounding boxes

--- task/analysis/wrapped_functions/pluginRegistryContains_wrapped.java
+++ task/analysis/wrapped_functions/pluginRegistryContains_wrapped.java
@@ -1,29 +1,32 @@
-public class PluginRegistry {
+import java.util.Iterator;
 
-    private List<PluginRegistryNode> registry;
-
-    public PluginRegistry() {
-        this.registry = new ArrayList<>();
-    }
-
-    public void addNode(PluginRegistryNode node) {
-        synchronized (registry) {
-            registry.add(node);
-        }
-    }
+public class MyClass {
+    // Assuming registry is a collection of PluginRegistryNode
+    Iterable<PluginRegistryNode> registry;
 
     public boolean pluginRegistryContains(String pluginName) {
         synchronized (registry) {
-            Iterator<PluginRegistryNode> iter;
+            Iterator iter;
 
             for (iter = registry.iterator(); iter.hasNext(); ) {
-                PluginRegistryNode node = iter.next();
-                if (node.plugin.getName().equals(pluginName)) {
-                    return true;
-                }
+                PluginRegistryNode node = (PluginRegistryNode) iter.next();
+                if (node.plugin.getName().equals(pluginName)) return true;
             }
         }
         return false;
     }
 
+    // Placeholder class definitions, replace these with your actual class definitions
+    class PluginRegistryNode {
+        // Your PluginRegistryNode class implementation goes here
+        Plugin plugin;
+    }
+
+    class Plugin {
+        // Your Plugin class implementation goes here
+        String getName() {
+            // Replace this with your actual implementation
+            return "";
+        }
+    }
 }
